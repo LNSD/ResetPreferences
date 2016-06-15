c900828f33ff89f6fed330d6da0d4dc7dd0ec545
Lorenzo Delgado <lorenzodelgado.dev@gmail.com>
2016-06-15 02:12:22 +0200

    Created data display layout

--- example/src/main/java/es/lnsd/example/MainActivity.java
+++ example/src/main/java/es/lnsd/example/MainActivity.java
@@ -1,33 +1,65 @@
 package es.lnsd.example;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import es.lnsd.example.settings.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE = 0x00;
+
+    //region Views
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Bind(R.id.boolean_switch)
+    Switch booleanInput;
+    @Bind(R.id.boolean_value)
+    TextView booleanValue;
+    @Bind(R.id.integer_input)
+    EditText integerInput;
+    @Bind(R.id.integer_value)
+    TextView integerValue;
+    @Bind(R.id.float_input)
+    EditText floatInput;
+    @Bind(R.id.float_value)
+    TextView floatValue;
+    @Bind(R.id.string_input)
+    EditText stringInput;
+    @Bind(R.id.string_value)
+    TextView stringValue;
+    //endregion
+
+    private static SharedPreferences sharedPreferences;
+    private static SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        updateTextviewValues();
     }
 
+    //region Toolbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -43,10 +75,103 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                Intent intent = new Intent(getBaseContext(), SettingsActivity.class);
+                startActivityForResult(intent, SettingsActivity.REQUEST_CODE);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    //endregion
+
+    @OnClick(R.id.apply_button)
+    public void onApplyButtonClick(Button button) {
+        updateStoredValues();
+        updateTextviewValues();
+        clearEditText();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case SettingsActivity.REQUEST_CODE:
+                if (resultCode == Activity.RESULT_OK) {
+                    updateTextviewValues();
+                }
+                break;
+            default:
+                //Do nothing
+        }
+    }
+
+    private void updateTextviewValues() {
+
+        if(!sharedPreferences.contains(getString(R.string.boolean_key))) {
+            booleanValue.setTextColor(getResources().getColor(android.R.color.holo_red_light));
+            booleanValue.setText("Unknown");
+        } else {
+            booleanValue.setTextColor(getResources().getColor(android.R.color.holo_green_dark));
+            boolean valueBoolean = sharedPreferences.getBoolean(getString(R.string.boolean_key), false);
+            booleanValue.setText((valueBoolean)?"true":"false");
         }
 
-        return super.onOptionsItemSelected(item);
+        if(!sharedPreferences.contains(getString(R.string.integer_key))) {
+            integerValue.setTextColor(getResources().getColor(android.R.color.holo_red_light));
+            integerValue.setText("Unknown");
+        } else {
+            integerValue.setTextColor(getResources().getColor(android.R.color.holo_green_dark));
+            int valueInteger = sharedPreferences.getInt(getString(R.string.integer_key), -1);
+            integerValue.setText(Integer.toString(valueInteger));
+        }
+
+        if(!sharedPreferences.contains(getString(R.string.float_key))) {
+            floatValue.setTextColor(getResources().getColor(android.R.color.holo_red_light));
+            floatValue.setText("Unknown");
+        } else {
+            floatValue.setTextColor(getResources().getColor(android.R.color.holo_green_dark));
+            float valueFloat = sharedPreferences.getFloat(getString(R.string.float_key), -1f);
+            floatValue.setText(Float.toString(valueFloat));
+        }
+
+        if(!sharedPreferences.contains(getString(R.string.string_key))) {
+            stringValue.setTextColor(getResources().getColor(android.R.color.holo_red_light));
+            stringValue.setText("Unknown");
+        } else {
+            stringValue.setTextColor(getResources().getColor(android.R.color.holo_green_dark));
+            String valueString = sharedPreferences.getString(getString(R.string.string_key), null);
+            stringValue.setText(valueString);
+        }
+    }
+
+    private void updateStoredValues() {
+        editor = sharedPreferences.edit();
+
+        editor.putBoolean(getString(R.string.boolean_key), booleanInput.isChecked());
+
+        String istring = integerInput.getText().toString();
+        if (!istring.isEmpty()) {
+            editor.putInt(getString(R.string.integer_key), Integer.parseInt(istring));
+        }
+
+        String fstring = floatInput.getText().toString();
+        if (!fstring.isEmpty()) {
+            editor.putFloat(getString(R.string.float_key), Float.parseFloat(fstring));
+        }
+
+        String string = stringInput.getText().toString();
+        if (!string.isEmpty()) {
+            editor.putString(getString(R.string.string_key), string);
+        }
+
+        editor.apply();
+    }
+
+    private void clearEditText() {
+        integerInput.setText("");
+        floatInput.setText("");
+        stringInput.setText("");
     }
 }
---
+++ example/src/main/java/es/lnsd/example/settings/SettingsActivity.java
@@ -0,0 +1,50 @@
+package es.lnsd.example.settings;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
+
+import es.lnsd.example.R;
+
+/**
+ * A {@link PreferenceActivity} that presents a set of application settings. On
+ * handset devices, settings are presented as a single list. On tablets,
+ * settings are split by category, with category headers shown to the left of
+ * the list of settings.
+ * <p/>
+ * See <a href="http://developer.android.com/design/patterns/settings.html">
+ * Android Design: Settings</a> for design guidelines and the <a
+ * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
+ * API Guide</a> for more information on developing a Settings UI.
+ */
+public class SettingsActivity extends AppCompatPreferenceActivity {
+    public static final int REQUEST_CODE = 0x01;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        addPreferencesFromResource(R.xml.settings);
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            if (!super.onMenuItemSelected(featureId, item)) {
+                NavUtils.navigateUpFromSameTask(this);
+            }
+            return true;
+        }
+        return super.onMenuItemSelected(featureId, item);
+    }
+}
